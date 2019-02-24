572f7764023aab473283e5db8b092a61b6dd4293
Chuoke <chuokechungyoung@gmail.com>
2019-02-24 23:13:02 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,8 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', '') @yield('title-gap', '-') {{ config('app.name') }}</title>
+<link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-    @yield('content')
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="/">{{ config('app.name') }}</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+                <li class="nav-item"><a href="/about" class="nav-link">关于</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        @guest
+            @include('partials._invite_to_join')
+        @endguest
+        @yield('content')
+    </div>
 </body>
 </html>
---
+++ resources/views/partials/_invite_to_join.blade.php
@@ -0,0 +1,10 @@
+<div class="jumbotron">
+    <h1>Hello，There You Are !</h1>
+    <p class="lead">
+        翻迎来到 <a href=""><span class="h2">{{ config('app.name') }}</span></a> !
+    </p>
+    <p>这是一个友好的社区。</p>
+    <p>
+        期待你的加入，<a class="btn btn-lg btn-success" href="#">立即行动</a> 吧 ！
+    </p>
+</div>
