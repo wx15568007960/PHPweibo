1c0c90ae43a7a0592f327717c6190d9b1cc3276f
Chuoke <chuokechungyoung@gmail.com>
2019-02-25 23:21:46 +0800

    调整欢迎部分

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -10,13 +10,12 @@
         <div class="collapse navbar-collapse" id="navbar-main">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="/">首页</a>
+                    <a class="nav-link" href="/">广场</a>
                 </li>
             </ul>
-            <ul class="navbar-nav justify-content-end">
-                <li class="nav-item"><a href="/login" class="nav-link">登 陆</a></li>
-                <li class="nav-item"><a href="/register" class="nav-link">注 册</a></li>
-            </ul>
+            
+            <a class="btn btn-sm btn-outline-warning mr-2" href="/login">登 陆</a>
+            <a class="btn btn-sm btn-outline-light" href="/register">注 册</a>
         </div>
     </div>
 </nav>
--- resources/views/partials/_invite_to_join.blade.php
+++ resources/views/partials/_invite_to_join.blade.php
@@ -1,10 +1,12 @@
-<div class="jumbotron">
+<div class="jumbotron bg-white shadow-sm">
     <h1>Hello，There You Are !</h1>
     <p class="lead">
         翻迎来到 <a href=""><span class="h2">{{ config('app.name') }}</span></a> !
     </p>
     <p>这是一个友好的社区。</p>
     <p>
-        期待你的加入，<a class="btn btn-lg btn-success" href="#">立即行动</a> 吧 ！
+        期待你的加入 ！
     </p>
+    <a class="btn btn-success rounded-pill mr-2" href="/login">立即加入</a>
+    <a class="btn btn-light rounded-pill" href="{{ route('about') }}">了解更多</a>
 </div>
