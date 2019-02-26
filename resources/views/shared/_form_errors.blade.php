ad425443f356377c9d4774faeb9a318e8983828b
Chuoke <chuokechungyoung@gmail.com>
2019-02-26 23:31:33 +0800

    中文错误提示信息

--- resources/views/shared/_form_errors.blade.php
+++ resources/views/shared/_form_errors.blade.php
@@ -1,7 +1,7 @@
 @if (count($errors) > 0)
     <div class="alert alert-danger">
         <ul>
-            @foreach ($errors as $error)
+            @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
             @endforeach
         </ul>
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -4,9 +4,9 @@
 
 @section('content')
     <div class="offset-md-2 col-md-8">
-        <div class="card">
-            <div class="card-header">
-                <h5>注册</h5>
+        <div class="card border-0 shadow">
+            <div class="card-header bg-primary">
+                <h5 class="mb-0 text-white">注册</h5>
             </div>
             <div class="card-body">
 
