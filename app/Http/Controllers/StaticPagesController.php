5cfef1ac1b70d1b0d1d69b0a283727c8d98f48f3
Chuoke <chuokechungyoung@gmail.com>
2019-03-05 23:25:25 +0800

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Status;
 
 class StaticPagesController extends Controller
 {
     public function home()
     {
-        return view('static_pages/home');
+        $statuses = Status::orderBy('created_at', 'desc')
+                            ->paginate(10);
+
+        return view('static_pages/home', compact('statuses'));
     }
 
     public function help()
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -7,5 +7,28 @@
         @include('partials._invite_to_join')
     @endguest
     
-    <h1>首页</h1>
+    <div class="row">
+        <div class="col-sm-12">
+            <hr>
+        </div>
+    </div>
+    <div class="row">
+        <div class="offset-md-2 col-md-8 col-sm-12">
+            <div class="row">
+                @forelse ($statuses as $status)
+                    <div class="col-sm-12 mt-3">
+                        @include('statuses._status', $status)
+                    </div>
+                @empty
+                    <div class="col-sm-12 mt-3">
+                        <p class="text-center">没有微博动态！</p>
+                    </div>
+                @endforelse
+        
+                <div class="col-sm-12 mt-3">
+                    {!! $statuses->render() !!}
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
